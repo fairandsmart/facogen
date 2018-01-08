fec176fff8353a6f2269db5ea2553d3273b77bcb
Jérôme Blanchard <jayblanc@gmail.com>
2018-01-08 09:45:56 +0100

    Try boxable

--- src/main/java/com/fairandsmart/invoices/InvoiceGenerator.java
+++ src/main/java/com/fairandsmart/invoices/InvoiceGenerator.java
@@ -1,8 +1,11 @@
 package com.fairandsmart.invoices;
 
+import be.quodlibet.boxable.BaseTable;
+import be.quodlibet.boxable.datatable.DataTable;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
@@ -27,19 +30,26 @@ public class InvoiceGenerator {
     public void generateInvoice(Model model, Path output) throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
+        page.setMediaBox(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
         document.addPage(page);
+
+        //Global values
+        float margin = 10;
+        float bottomMargin = 0;
 
         PDFont font = PDType1Font.HELVETICA_BOLD;
         PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
+
+        //Logo
         String logo = this.getClass().getClassLoader().getResource("logo/logo.png").getFile();
         PDImageXObject pdImage = PDImageXObject.createFromFile(logo, document);
+        contentStream.drawImage(pdImage, margin, 700, pdImage.getWidth() / 4, pdImage.getHeight() / 4);
 
-        contentStream.drawImage(pdImage, 20, 700, pdImage.getWidth() / 4, pdImage.getHeight() / 4);
-
+        //Company address
         contentStream.beginText();
         contentStream.setFont(font, 12);
-        contentStream.newLineAtOffset(36, 700);
+        contentStream.newLineAtOffset(margin, 700);
         contentStream.showText("Fair and Smart SAS");
         contentStream.newLineAtOffset(0, -16);
         contentStream.showText("11 Rempart St Thiébault");
@@ -47,6 +57,9 @@ public class InvoiceGenerator {
         contentStream.showText("57000 Metz - France");
         contentStream.endText();
 
+
+
+        //Invoice Number
         contentStream.beginText();
         contentStream.setFont(font, 20);
         contentStream.setNonStrokingColor(Color.ORANGE);
@@ -54,7 +67,16 @@ public class InvoiceGenerator {
         contentStream.showText("INVOICE N°1276551");
         contentStream.endText();
 
+        //Invoice content
+        //Initialize table
+        float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
+        float yStartNewPage = page.getMediaBox().getHeight() - (2 * margin);
+        float yStart = yStartNewPage;
 
+        BaseTable pdfTable = new BaseTable(yStart, yStartNewPage, bottomMargin, tableWidth, margin, document, page, true,true);
+        DataTable table = new DataTable(pdfTable, page);
+        table.addListToTable();
+        pdfTable.draw();
 
         contentStream.close();
 
@@ -65,5 +87,7 @@ public class InvoiceGenerator {
     public enum Model {
         BASIC
     }
+
+
 
 }
