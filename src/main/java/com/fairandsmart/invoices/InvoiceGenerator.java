d95fc7bdaa4893d09a9d5d362152dd7791a49a3b
Jérôme Blanchard <jayblanc@gmail.com>
2018-01-08 10:11:27 +0100

    Add sample table

--- src/main/java/com/fairandsmart/invoices/InvoiceGenerator.java
+++ src/main/java/com/fairandsmart/invoices/InvoiceGenerator.java
@@ -2,17 +2,22 @@ package com.fairandsmart.invoices;
 
 import be.quodlibet.boxable.BaseTable;
 import be.quodlibet.boxable.datatable.DataTable;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
-import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InvoiceGenerator {
 
@@ -30,7 +35,7 @@ public class InvoiceGenerator {
     public void generateInvoice(Model model, Path output) throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
-        page.setMediaBox(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
+        //page.setMediaBox(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
         document.addPage(page);
 
         //Global values
@@ -44,7 +49,7 @@ public class InvoiceGenerator {
         //Logo
         String logo = this.getClass().getClassLoader().getResource("logo/logo.png").getFile();
         PDImageXObject pdImage = PDImageXObject.createFromFile(logo, document);
-        contentStream.drawImage(pdImage, margin, 700, pdImage.getWidth() / 4, pdImage.getHeight() / 4);
+        contentStream.drawImage(pdImage, margin, 715, pdImage.getWidth() / 4, pdImage.getHeight() / 4);
 
         //Company address
         contentStream.beginText();
@@ -57,12 +62,9 @@ public class InvoiceGenerator {
         contentStream.showText("57000 Metz - France");
         contentStream.endText();
 
-
-
         //Invoice Number
         contentStream.beginText();
         contentStream.setFont(font, 20);
-        contentStream.setNonStrokingColor(Color.ORANGE);
         contentStream.newLineAtOffset(400, 750);
         contentStream.showText("INVOICE N°1276551");
         contentStream.endText();
@@ -71,12 +73,16 @@ public class InvoiceGenerator {
         //Initialize table
         float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
         float yStartNewPage = page.getMediaBox().getHeight() - (2 * margin);
-        float yStart = yStartNewPage;
-
-        BaseTable pdfTable = new BaseTable(yStart, yStartNewPage, bottomMargin, tableWidth, margin, document, page, true,true);
-        DataTable table = new DataTable(pdfTable, page);
-        table.addListToTable();
-        pdfTable.draw();
+        float yStart = 450;
+        List<List> data = new ArrayList();
+        data.add(new ArrayList<>( Arrays.asList("Column One", "Column Two", "Column Three", "Column Four", "Column Five")));
+        for (int i = 1; i <= 10; i++) {
+            data.add(new ArrayList<>( Arrays.asList("Row " + i + " Col One", "Row " + i + " Col Two", "Row " + i + " Col Three", "Row " + i + " Col Four", "Row " + i + " Col Five")));
+        }
+        BaseTable dataTable = new BaseTable(yStart, yStartNewPage, bottomMargin, tableWidth, margin, document, page, true, true);
+        DataTable t = new DataTable(dataTable, page);
+        t.addListToTable(data, DataTable.HASHEADER);
+        dataTable.draw();
 
         contentStream.close();
 
