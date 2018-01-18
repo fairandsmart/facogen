9bd10765655b93ee8816d823cdcb689786e52b27
jean-baptiste  manet <jean-baptiste.manet@intech.lu>
2018-01-18 15:23:47 +0100

    Company Class

--- src/main/java/com/fairandsmart/invoices/data/model/Company.java
+++ src/main/java/com/fairandsmart/invoices/data/model/Company.java
@@ -1,10 +1,43 @@
 package com.fairandsmart.invoices.data.model;
 
+import com.fairandsmart.invoices.data.generator.GenerationContext;
+import com.fairandsmart.invoices.data.generator.ModelGenerator;
+
 public class Company {
+
 
     private Logo logo;
 
-    /*
+
+    public Company() {
+    }
+
+    public Logo getLogo() {
+        return logo;
+    }
+
+    public void setLogo(Logo logo) {
+        this.logo = logo;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "logo=" + logo +
+                '}';
+    }
+
+    public static class Generator implements ModelGenerator<Company> {
+
+        @Override
+        public Company generate(GenerationContext ctx) {
+            Company model = new Company();
+            model.setLogo(new Logo.Generator().generate(ctx));
+            return model;
+        }
+    }
+
+/*
     private String name;
     private Address address;
     private String fax;
