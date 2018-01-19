d81408c64063a41e0b5140f921ddf344dd841c11
jean-baptiste  manet <jean-baptiste.manet@intech.lu>
2018-01-19 13:15:06 +0100

    ProductContainer

--- src/main/java/com/fairandsmart/invoices/data/model/Product.java
+++ src/main/java/com/fairandsmart/invoices/data/model/Product.java
@@ -2,16 +2,163 @@ package com.fairandsmart.invoices.data.model;
 
 public class Product {
 
-    private String name;
+
     private String description;
-    private String serial;
-    private float quantity;
-    private float grossAmount;
-    private float netAmount;
     private String taxType;
+
+    private String name;
+    private String ean;
+    private String sku;
+    private String brand;
+    private float priceWithTax;
+    private float priceWithTaxDisplay;
+    private float priceWithoutTax;
+    private float priceWithoutTaxDisplay;
+    private int quantity;
     private float taxRate;
-    private float taxAmount;
+    private float totalPriceWithTax;
+    private float totalPriceWithoutTax;
+    private float totalTax;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTaxType() {
+        return taxType;
+    }
+
+    public void setTaxType(String taxType) {
+        this.taxType = taxType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPriceWithTax() {
+        return priceWithTax;
+    }
+
+    public void setPriceWithTax(float priceWithTax) {
+        this.priceWithTax = priceWithTax;
+    }
+
+    public float getPriceWithTaxDisplay() {
+        return priceWithTaxDisplay;
+    }
+
+    public void setPriceWithTaxDisplay(float priceWithTaxDisplay) {
+        this.priceWithTaxDisplay = priceWithTaxDisplay;
+    }
+
+    public float getPriceWithoutTax() {
+        return priceWithoutTax;
+    }
+
+    public void setPriceWithoutTax(float priceWithoutTax) {
+        this.priceWithoutTax = priceWithoutTax;
+    }
+
+    public float getPriceWithoutTaxDisplay() {
+        return priceWithoutTaxDisplay;
+    }
+
+    public void setPriceWithoutTaxDisplay(float priceWithoutTaxDisplay) {
+        this.priceWithoutTaxDisplay = priceWithoutTaxDisplay;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(float taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public float getTotalPriceWithTax() {
+        return priceWithTax * quantity;
+    }
+
+    public float getTotalPriceWithoutTax() {
+        return priceWithoutTax * quantity;
+    }
+
+    public float getTotalTax() {
+        return this.getTotalPriceWithTax() - this.getTotalPriceWithoutTax();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "description='" + description + '\'' +
+                ", taxType='" + taxType + '\'' +
+                ", name='" + name + '\'' +
+                ", ean='" + ean + '\'' +
+                ", sku='" + sku + '\'' +
+                ", brand='" + brand + '\'' +
+                ", priceWithTax=" + priceWithTax +
+                ", priceWithTaxDisplay=" + priceWithTaxDisplay +
+                ", priceWithoutTax=" + priceWithoutTax +
+                ", priceWithoutTaxDisplay=" + priceWithoutTaxDisplay +
+                ", quantity=" + quantity +
+                ", taxRate=" + taxRate +
+                '}';
+    }
 
 
+    /*
+      "price": "479.0",
+              "ean": "8806088499048",
+              "name": "Lave linge hublot Samsung ADD WASH WW90K4437YW",
+              "brand": "Samsung",
+              "sku": "000000000001079579",
+              "categories": [
+              "Gros électroménager",
+              "Lave-linge",
+              "Lave-linge hublot"
+              ]
+
+              */
 
 }
--- src/main/java/com/fairandsmart/invoices/data/model/ProductContainer.java
+++ src/main/java/com/fairandsmart/invoices/data/model/ProductContainer.java
@@ -20,14 +20,56 @@ import java.util.stream.Collectors;
 
 public class ProductContainer {
 
+    private List<Product> products = new ArrayList<Product>();
+    private float totalWithTax;
+    private float totalWithoutTax;
+
     public ProductContainer() {
-    }
-
-    public void generateRandomProduct() throws Exception {
-
 
     }
 
+    public void addProduct(Product product) {
+        products.add(product);
+        totalWithTax = totalWithTax + ( product.getQuantity() * product.getPriceWithTax());
+        totalWithoutTax = totalWithoutTax + ( product.getQuantity() * product.getPriceWithoutTax());
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public float getTotalWithTax() {
+        return totalWithTax;
+    }
+
+    public void setTotalWithTax(float totalWithTax) {
+        this.totalWithTax = totalWithTax;
+    }
+
+    public float getTotalWithoutTax() {
+        return totalWithoutTax;
+    }
+
+    public void setTotalWithoutTax(float totalWithoutTax) {
+        this.totalWithoutTax = totalWithoutTax;
+    }
+
+    public float getTotalTax() {
+        return totalWithTax - totalWithoutTax;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductContainer{" +
+                "products=" + products +
+                ", totalWithTax=" + totalWithTax +
+                ", totalWithoutTax=" + totalWithoutTax +
+                '}';
+    }
 
     public static class Generator implements ModelGenerator<ProductContainer> {
 
@@ -42,8 +84,16 @@ public class ProductContainer {
 
         @Override
         public ProductContainer generate(GenerationContext ctx) {
-            Product electibleLogo = products.get(ctx.getRandom().nextInt(products.size()));
-            return new ProductContainer();
+
+            int maxProduct = 4;
+            ProductContainer productContainer = new ProductContainer();
+            for (int i = 0; i < ctx.getRandom().nextInt(maxProduct -1)+1; i++) {
+                int maxQuantity = 5;
+                Product electibleProduct = products.get(ctx.getRandom().nextInt(products.size()));
+                electibleProduct.setQuantity(ctx.getRandom().nextInt(maxQuantity -1) +1);
+                productContainer.addProduct(electibleProduct);
+            }
+            return productContainer;
         }
 
     }
