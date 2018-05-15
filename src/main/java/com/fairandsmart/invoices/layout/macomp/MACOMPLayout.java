a30701b7762143bce90b459556a60e38ee375fa5
ldevashi <devashishlohani@gmail.com>
2018-05-15 12:17:32 +0200

    This layout contains dates, client info, company info and various ID numbers generated.

--- src/main/java/com/fairandsmart/invoices/layout/macomp/MACOMPLayout.java
+++ src/main/java/com/fairandsmart/invoices/layout/macomp/MACOMPLayout.java
@@ -23,6 +23,59 @@ import java.util.Random;
 
 public class MACOMPLayout implements InvoiceLayout {
 
+    private static List<String> iDate_heads_fr = new ArrayList<>();
+    private static List<String> iDate_heads_en = new ArrayList<>();
+    private static List<String> iNum_heads_fr = new ArrayList<>();
+    private static List<String> iNum_heads_en = new ArrayList<>();
+    private static List<String> cNum_heads_fr = new ArrayList<>();
+    private static List<String> cNum_heads_en = new ArrayList<>();
+    private static List<String> paymentInfo_heads_fr = new ArrayList<>();
+    private static List<String> paymentInfo_heads_en = new ArrayList<>();
+
+    {
+        iDate_heads_fr.add("Date de la facture");
+        iDate_heads_fr.add("Date");
+    }
+
+    {
+        iDate_heads_en.add("Date");
+        iDate_heads_en.add("Invoice Date");
+    }
+
+    {
+        iNum_heads_fr.add("Facture N°");
+        iNum_heads_fr.add("Numéro de facture");
+        iNum_heads_fr.add("Référence de la facture");
+    }
+
+    {
+        iNum_heads_en.add("Invoice Number");
+        iNum_heads_en.add("Invoice Reference");
+    }
+
+    {
+        cNum_heads_fr.add("Numéro de client");
+        cNum_heads_fr.add("Client N°");
+        cNum_heads_fr.add("Référence de la client");
+    }
+
+    {
+        cNum_heads_en.add("Client Number");
+        cNum_heads_en.add("Client Reference");
+        cNum_heads_en.add("Ref Client");
+    }
+
+    {
+        paymentInfo_heads_fr.add("Mode de règlement");
+        paymentInfo_heads_fr.add("Moyen de paiement");
+    }
+
+    {
+        paymentInfo_heads_en.add("Payment means");
+        paymentInfo_heads_en.add("Payed through");
+        paymentInfo_heads_en.add("Mode of Payment");
+    }
+
     private static final List<PDFont[]> FONTS = new ArrayList<>();
     {
         FONTS.add(new PDFont[] {PDType1Font.HELVETICA, PDType1Font.HELVETICA_BOLD, PDType1Font.HELVETICA_OBLIQUE} );
@@ -33,6 +86,7 @@ public class MACOMPLayout implements InvoiceLayout {
     @Override
     public void builtInvoice(InvoiceModel model, PDDocument document, XMLStreamWriter writer) throws Exception {
 
+        float fontSize = 10;
         PDPage page = new PDPage(PDRectangle.A4);
         document.addPage(page);
         writer.writeStartElement("DL_PAGE");
@@ -48,24 +102,55 @@ public class MACOMPLayout implements InvoiceLayout {
         new SimpleTextBox(fonts[1], 13, 350, 748, "Facture No. "+ model.getReference().getValue()).build(contentStream, writer);
 
         // Company Info Box
-        CompanyInfoBox companyInfoBox = new CompanyInfoBox(fonts[2], fonts[1], 10, model, document);
+        CompanyInfoBox companyInfoBox = new CompanyInfoBox(fonts[2], fonts[1], fontSize, model, document);
         companyInfoBox.build(contentStream, writer);
         companyInfoBox.translate(50, 785);
         companyInfoBox.build(contentStream, writer);
 
         // Billing Address Block
-        ClientInfoBox billingInfoBox = new ClientInfoBox(fonts[2], fonts[1], 10, model, document, "Billing");
+        ClientInfoBox billingInfoBox = new ClientInfoBox(fonts[2], fonts[1], fontSize, model, document, "Billing");
         billingInfoBox.build(contentStream, writer);
         billingInfoBox.translate(50, 650);
         billingInfoBox.build(contentStream, writer);
 
         // Shipping Address Block
-        ClientInfoBox shippingInfoBox = new ClientInfoBox(fonts[2], fonts[1], 10, model, document, "Shipping");
+        ClientInfoBox shippingInfoBox = new ClientInfoBox(fonts[2], fonts[1], fontSize, model, document, "Shipping");
         shippingInfoBox.build(contentStream, writer);
         shippingInfoBox.translate(50, 580);
         shippingInfoBox.build(contentStream, writer);
 
+        VerticalContainer invoiceInfo = new VerticalContainer(300, 650, 400);
+        SimpleTextBox dateHead = new SimpleTextBox(fonts[1], fontSize+1, 0,0, iDate_heads_fr.get(random.nextInt(iDate_heads_fr.size())));
+        invoiceInfo.addElement(dateHead);
 
+        SimpleTextBox idate = new SimpleTextBox(fonts[0], fontSize, 330,650, model.getDate().getValue());
+        invoiceInfo.addElement(idate);
+
+        SimpleTextBox vatHead = new SimpleTextBox(fonts[1], fontSize+1, 0,0, model.getCompany().getIdNumbers().getVatLabel());
+        invoiceInfo.addElement(vatHead);
+
+        SimpleTextBox svat = new SimpleTextBox(fonts[0], fontSize, 330,0, model.getCompany().getIdNumbers().getVatValue());
+        invoiceInfo.addElement(svat);
+
+        SimpleTextBox cidHead = new SimpleTextBox(fonts[1], fontSize+1, 0,0, model.getCompany().getIdNumbers().getCidLabel());
+        invoiceInfo.addElement(cidHead);
+
+        SimpleTextBox scid = new SimpleTextBox(fonts[0], fontSize, 330,0, model.getCompany().getIdNumbers().getCidValue());
+        invoiceInfo.addElement(scid);
+
+        SimpleTextBox siretHead = new SimpleTextBox(fonts[1], fontSize+1, 0,0, model.getCompany().getIdNumbers().getSiretLabel());
+        invoiceInfo.addElement(siretHead);
+
+        SimpleTextBox ssiret = new SimpleTextBox(fonts[0], fontSize, 0,0, model.getCompany().getIdNumbers().getSiretValue());
+        invoiceInfo.addElement(ssiret);
+
+        SimpleTextBox toaHead = new SimpleTextBox(fonts[1], fontSize+1, 0,0, model.getCompany().getIdNumbers().getToaLabel());
+        invoiceInfo.addElement(toaHead);
+
+        SimpleTextBox stoa = new SimpleTextBox(fonts[0], fontSize, 0,0, model.getCompany().getIdNumbers().getToaValue());
+        invoiceInfo.addElement(stoa);
+
+        invoiceInfo.build(contentStream, writer);
         contentStream.close();
 
     }
